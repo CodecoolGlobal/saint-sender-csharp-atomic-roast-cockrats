e3f7fff2ab7de7eb7ea3d9f05e653f6d2166b654
nagyakornel <nagyakornel@gmail.com>
2020-08-10 13:30:20 +0200

    Created basic buttons and Grid for the emails.

--- SaintSender.DesktopUI/Views/MainWindow.xaml.cs
+++ SaintSender.DesktopUI/Views/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using SaintSender.Core.Services;
+﻿using System.Windows;
 
 namespace SaintSender.DesktopUI
 {
@@ -24,14 +10,6 @@ namespace SaintSender.DesktopUI
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void GreetBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var service = new GreetService();
-            var name = NameTxt.Text;
-            var greeting = service.Greet(name);
-            ResultTxt.Text = greeting;
         }
     }
 }
