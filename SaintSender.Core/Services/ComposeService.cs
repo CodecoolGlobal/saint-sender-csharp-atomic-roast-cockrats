aa2c5f278eece18a9f55991bdc12820d4bbd91d2
dobitamas <dobi.tamas0428@gmail.com>
2020-08-11 11:16:50 +0200

    Created a new Mail Object with the required fields and a form which creates it

---
+++ SaintSender.Core/Services/ComposeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SaintSender.Core.Entities;
+using SaintSender.Core.Interfaces;
+
+namespace SaintSender.Core.Services
+{
+    public class ComposeService : IComposeService
+    {
+        public void Compose(MailComposeModel mailComposeModel)
+        {
+            Console.WriteLine(mailComposeModel.ToString());
+        }
+    }
+}
---
+++ SaintSender.DesktopUI/Views/ComposeMail.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SaintSender.DesktopUI.ViewModels;
+
+namespace SaintSender.DesktopUI.Views
+{
+    /// <summary>
+    /// Interaction logic for ComposeMail.xaml
+    /// </summary>
+    public partial class ComposeMail : Window
+    {
+        #region Private Properties
+
+        private readonly ComposeMailViewModel _composeMailViewModel;
+
+        #endregion Private Properties
+
+        #region Constructor
+
+        public ComposeMail()
+        {
+            InitializeComponent();
+            _composeMailViewModel = new ComposeMailViewModel();
+            DataContext = _composeMailViewModel;
+        }
+
+        #endregion Constructor
+
+        #region Event Handlers
+
+        private void ComposeWindowCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ComposeButton_Click(object sender, RoutedEventArgs e)
+        {
+            _composeMailViewModel.Compose();
+        }
+
+        #endregion Event Handlers
+    }
+}
